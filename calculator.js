ec8a0a439de17ef64240797738627e8939baa94c
Sahil Gautam <132191838+Unseencoderz@users.noreply.github.com>
2025-07-02 13:50:18 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,35 +1,55 @@
 function appendToDisplay(value) {
-    document.getElementById("display").value += value;
+  document.getElementById("display").value += value;
 }
 
 function clearDisplay() {
-    document.getElementById("display").value = "";
+  document.getElementById("display").value = "";
 }
 
 function calculateResult() {
-    try {
-        document.getElementById("display").value = eval(document.getElementById("display").value);
-    } catch (error) {
-        document.getElementById("display").value = "Error";
-    }
+  try {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
 
 function calculateSquareRoot() {
+  try {
     document.getElementById("display").value = Math.sqrt(eval(document.getElementById("display").value));
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
 
 function calculatePower() {
+  try {
     document.getElementById("display").value = Math.pow(eval(document.getElementById("display").value), 2);
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
 
 function calculateSin() {
-    document.getElementById("display").value = Math.sin(eval(document.getElementById("display").value));
+  try {
+    document.getElementById("display").value = Math.sin(eval(document.getElementById("display").value)).toFixed(4);
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
 
 function calculateCos() {
-    document.getElementById("display").value = Math.cos(eval(document.getElementById("display").value));
+  try {
+    document.getElementById("display").value = Math.cos(eval(document.getElementById("display").value)).toFixed(4);
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
 
 function calculateTan() {
-    document.getElementById("display").value = Math.tan(eval(document.getElementById("display").value));
+  try {
+    document.getElementById("display").value = Math.tan(eval(document.getElementById("display").value)).toFixed(4);
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
